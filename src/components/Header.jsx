d1f0f480c96f818e7011ffd8e05c4e76a4ac5488
Rafael Oliveira <aquinorafaeldev@gmail.com>
2025-02-14 17:27:31 -0300

    feat: Main da tela principal desenvolvido.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import logoSpotify from "../assets/logo/spotify-logo.png";
+
 const Header = () => {
   return (
-    <>
-      <header>
-        <img src={logoSpotify} alt="Logo do Spotify" />
-        <a href="/">
-          <h1>Spotify</h1>
-        </a>
-      </header>
-    </>
+    <div className="header">
+      <img src={logoSpotify} alt="Logo do Spotify" />
+
+      <a className="header__link" href="/">
+        <h1>Spotify</h1>
+      </a>
+    </div>
   );
 };
 
