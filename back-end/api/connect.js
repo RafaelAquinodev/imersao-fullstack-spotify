5f17b0f3d2c80660a7a94cea8087e0bd1706f4c2
Rafael Oliveira <aquinorafaeldev@gmail.com>
2025-02-17 12:01:16 -0300

    adicionando .env

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,7 +1,11 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-const URI =
-  "mongodb+srv://aquinorafaeldev:Le%40f2598@cluster0.zxdev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config();
+
+// Usa a variável de ambiente MONGODB_URI
+const URI = process.env.MONGODB_URI;
 
 const client = new MongoClient(URI);
 
